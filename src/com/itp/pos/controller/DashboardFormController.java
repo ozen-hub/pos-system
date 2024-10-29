90369bc6ce831df38a4f3fc82bf875372868e1e0
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-29 16:06:49 +0530

    created register form

--- src/com/itp/pos/controller/DashboardFormController.java
+++ src/com/itp/pos/controller/DashboardFormController.java
@@ -1,4 +1,45 @@
 package com.itp.pos.controller;
 
+import com.itp.pos.db.Database;
+import com.itp.pos.model.User;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.control.Label;
+import javafx.util.Duration;
+
+import java.time.LocalTime;
+
 public class DashboardFormController {
+    public Label lblUser;
+    public Label lblTime;
+
+    public void initialize() {
+        loadUser();
+        loadDateAndTime();
+    }
+
+    private void loadDateAndTime() {
+        // time
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.ZERO,e->{
+                    LocalTime localTime = LocalTime.now();
+                    lblTime.setText(
+                    localTime.getHour()+":"+
+                    localTime.getMinute()+":"+
+                    localTime.getSecond()
+                    );
+                }),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void loadUser() {
+        User user = Database.user;
+        lblUser.setText(
+                user.getFullName()
+        );
+    }
 }
