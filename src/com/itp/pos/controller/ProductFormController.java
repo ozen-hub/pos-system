a21a7a0b9b47d2d866d2afd19a8c20a7842f31ba
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-31 10:29:46 +0530

    finalize

--- src/com/itp/pos/controller/ProductFormController.java
+++ src/com/itp/pos/controller/ProductFormController.java
@@ -6,12 +6,11 @@ import com.itp.pos.view.tm.ProductTm;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+
+import java.util.Optional;
 
 public class ProductFormController {
     public AnchorPane context;
@@ -28,7 +27,7 @@ public class ProductFormController {
     public TableColumn colQty;
     public TableColumn colTools;
 
-    private String searchText="";
+    private String searchText = "";
 
     public void initialize() {
         loadTableData(searchText);
@@ -43,10 +42,11 @@ public class ProductFormController {
 
     private ObservableList<ProductTm> obList =
             FXCollections.observableArrayList();
+
     private void loadTableData(String searchText) {
-        searchText=searchText.toLowerCase();
-        for(Product p:Database.productTable){
-            if(p.getDescription().toLowerCase().contains(searchText)){
+        searchText = searchText.toLowerCase();
+        for (Product p : Database.productTable) {
+            if (p.getDescription().toLowerCase().contains(searchText)) {
                 Button btn = new Button("Delete");
                 ProductTm tm = new ProductTm(
                         p.getProductId(),
@@ -55,6 +55,28 @@ public class ProductFormController {
                         p.getQtyOnHand(),
                         btn
                 );
+
+                String finalSearchText = searchText;
+                btn.setOnAction((e) -> {
+
+                    Alert alert = new Alert(Alert.AlertType.CONFIRMATION,
+                            "Are you sure? whether Do you want to delete this Product?",
+                            ButtonType.YES, ButtonType.NO, ButtonType.CLOSE);
+
+                    Optional<ButtonType> buttonType =
+                            alert.showAndWait();
+                    if (buttonType.get().equals(ButtonType.YES)) {
+                        for (Product pro : Database.productTable) {
+                            if (pro.getProductId().equals(tm.getId())) {
+                                Database.productTable.remove(pro);
+                                loadTableData(finalSearchText);
+                                return;
+                            }
+                        }
+                    }
+
+                });
+
                 obList.add(tm);
                 tblProducts.setItems(obList);
             }
@@ -68,5 +90,41 @@ public class ProductFormController {
     }
 
     public void saveOnAction(ActionEvent actionEvent) {
+        Product product = new Product(
+                txtId.getText(),
+                txtDescription.getText(),
+                Double.parseDouble(txtUnitPrice.getText()),
+                Integer.parseInt(txtQty.getText())
+        );
+
+        if (btnSave.getText().equals("Save Product")) {
+            Database.productTable.add(product);
+            new Alert(Alert.AlertType.INFORMATION, "Saved..").show();
+            loadTableData(searchText);
+        } else {
+            for (Product p : Database.productTable) {
+                if (p.getProductId().equals(product.getProductId())) {
+                    p.setDescription(product.getDescription());
+                    p.setQtyOnHand(product.getQtyOnHand());
+                    p.setUitPrice(product.getUitPrice());
+                }
+                loadTableData(searchText);
+                new Alert(Alert.AlertType.INFORMATION,
+                        "Success").show();
+                btnSave.setText("Save Product");
+                clear();
+                txtId.setEditable(true);
+                return;
+            }
+
+        }
+
+    }
+    private void clear() {
+        txtId.clear();
+        txtDescription.clear();
+        txtUnitPrice.clear();
+        txtQty.clear();
+        txtId.requestFocus();
     }
 }
