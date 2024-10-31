1053c87858fa6d122822fb214485e13857d78325
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-31 11:57:28 +0530

    finalize

--- src/com/itp/pos/controller/PlaceOrderFormController.java
+++ src/com/itp/pos/controller/PlaceOrderFormController.java
@@ -2,15 +2,27 @@ package com.itp.pos.controller;
 
 import com.itp.pos.db.Database;
 import com.itp.pos.model.Customer;
+import com.itp.pos.model.Product;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ComboBox;
 
 public class PlaceOrderFormController {
     public ComboBox<String> cmbCustomerId;
+    public ComboBox<String> cmbProductId;
 
     public void initialize() {
         loadCustomerIds();
+        loadAllProductIds();
+    }
+
+    private void loadAllProductIds() {
+        ObservableList<String> obList =
+                FXCollections.observableArrayList();
+        for(Product p: Database.productTable){
+            obList.add(p.getProductId());
+        }
+        cmbProductId.setItems(obList);
     }
 
     private void loadCustomerIds() {
