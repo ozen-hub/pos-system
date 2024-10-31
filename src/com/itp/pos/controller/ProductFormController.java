b1589949d3b388637f3974c0ff2489eb1c396640
education-bootcamp <education.seekerscloud@gmail.com>
2024-10-31 10:16:31 +0530

    finalize

--- src/com/itp/pos/controller/ProductFormController.java
+++ src/com/itp/pos/controller/ProductFormController.java
@@ -1,11 +1,16 @@
 package com.itp.pos.controller;
 
+import com.itp.pos.db.Database;
+import com.itp.pos.model.Product;
 import com.itp.pos.view.tm.ProductTm;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 
 public class ProductFormController {
@@ -22,6 +27,39 @@ public class ProductFormController {
     public TableColumn colUnitPrice;
     public TableColumn colQty;
     public TableColumn colTools;
+
+    private String searchText="";
+
+    public void initialize() {
+        loadTableData(searchText);
+
+        colId.setCellValueFactory(new PropertyValueFactory<ProductTm, String>("id"));
+        colDescription.setCellValueFactory(new PropertyValueFactory<ProductTm, String>("description"));
+        colTools.setCellValueFactory(new PropertyValueFactory<ProductTm, String>("button"));
+        colUnitPrice.setCellValueFactory(new PropertyValueFactory<ProductTm, String>("unitPrice"));
+        colQty.setCellValueFactory(new PropertyValueFactory<ProductTm, String>("qty"));
+
+    }
+
+    private ObservableList<ProductTm> obList =
+            FXCollections.observableArrayList();
+    private void loadTableData(String searchText) {
+        searchText=searchText.toLowerCase();
+        for(Product p:Database.productTable){
+            if(p.getDescription().toLowerCase().contains(searchText)){
+                Button btn = new Button("Delete");
+                ProductTm tm = new ProductTm(
+                        p.getProductId(),
+                        p.getDescription(),
+                        p.getUitPrice(),
+                        p.getQtyOnHand(),
+                        btn
+                );
+                obList.add(tm);
+                tblProducts.setItems(obList);
+            }
+        }
+    }
 
     public void backToHomeOnAction(ActionEvent actionEvent) {
     }
