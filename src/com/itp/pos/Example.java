6758aaf8ba221cafd8384f509bbbcdd95071e1fc
education-bootcamp <education.seekerscloud@gmail.com>
2024-11-20 12:50:39 +0530

    ggg

--- src/com/itp/pos/Example.java
+++ src/com/itp/pos/Example.java
@@ -1,14 +1,60 @@
 package com.itp.pos;
 
 
+import com.itp.pos.db.DBConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public class Example {
     public static void main(String[] args) {
-        // StringBuilder & StringBuffer
-        StringBuilder builder =
-                new StringBuilder("dsfdsf");
-        StringBuffer buffer =
-                new StringBuffer("dsfdsf");
+        try {
+            Connection c = DBConnection.getInstance()
+                    .getConnection();
+            c.setAutoCommit(false);
+            if (saveStudent()) {
+                if (saveRegistration()) {
+                    c.commit();
+                } else {c.rollback();}
+            }else{c.rollback();}
 
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
 
+        }
     }
+
+
+    private static boolean saveStudent() throws SQLException, ClassNotFoundException {
+        PreparedStatement stm =
+                DBConnection
+                        .getInstance()
+                        .getConnection()
+                        .prepareStatement(
+                                "INSERT INTO student VALUES(?,?,?)"
+                        );
+        stm.setString(1, "1001");
+        stm.setString(2, "Saman Pradeep");
+        stm.setInt(3, 23);
+        return stm.executeUpdate() > 0;
+    }
+
+    private static boolean saveRegistration() throws SQLException, ClassNotFoundException {
+        PreparedStatement stm =
+                DBConnection
+                        .getInstance()
+                        .getConnection()
+                        .prepareStatement(
+                                "INSERT INTO registration VALUES(?,?,?,?)"
+                        );
+        stm.setString(1, "001");
+        stm.setObject(2, LocalDate.now());
+        stm.setDouble(3, 50000);
+        stm.setString(4, "1001");
+
+        return stm.executeUpdate() > 0;
+    }
+
 }
